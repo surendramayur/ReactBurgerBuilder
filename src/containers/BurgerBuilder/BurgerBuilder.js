233262103df0e3eeeef5ee715d16b40983593e50
Mayur <surendramayur@gmail.com>
2018-09-28 18:54:08 +0530

    calculaton ingrdient sum dynamically and handling zero ingredient case

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,10 +11,10 @@ class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            cheese: 0,
+            bacon: 0,
+            salad: 0,
+            meat: 0
         }
     }
 
