0e4ea69c0ab1d10998cddfd800a1f3d4dc6d6fb6
Mayur <surendramayur@gmail.com>
2018-10-19 12:40:38 +0530

    folder structure update putting layout in hoc

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import Aux from '../../hoc/ReactAux';
+import Aux from '../../hoc/ReactAux/ReactAux';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
